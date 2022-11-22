d3fa469bb93835e3b3a903fb996fd673e11a9ab6
José Esteves <jose.esteves@gmail.com>
2022-11-22 11:38:19 +0000

    C004: Animation redraw

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
 	draw();
 });
-
+var pos = 0;
 function draw() {
-	// Obtain a reference to the canvas element using its id.
+	// Request redraw after this has finish
+	requestAnimationFrame(draw);
+
+	// Get canvas and context element
 	htmlCanvas = document.getElementById('c1');
-	// Obtain a graphics context on the canvas element for drawing.
 	ctx = htmlCanvas.getContext('2d');
+
+	// Resize to the size of the canvas
 	ctx.canvas.width = window.innerWidth;
 	ctx.canvas.height = window.innerHeight;
-	//...drawing code...
+
+	// Clear the canvas
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+	// Draw section
+	ctx.fillStyle = 'yellow';
+	pos += 1;
+	ctx.fillRect(pos, pos, 50, 50);
 }
