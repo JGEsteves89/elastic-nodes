a141a1a049c7194a68872c1281910091578b94b5
José Esteves <jose.esteves@gmail.com>
2022-11-22 11:32:14 +0000

    C003: Simple canvas drawing

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello darkness my old friend');
+document.addEventListener('DOMContentLoaded', function () {
+	draw();
+});
+
+function draw() {
+	// Obtain a reference to the canvas element using its id.
+	htmlCanvas = document.getElementById('c1');
+	// Obtain a graphics context on the canvas element for drawing.
+	ctx = htmlCanvas.getContext('2d');
+	ctx.canvas.width = window.innerWidth;
+	ctx.canvas.height = window.innerHeight;
+	//...drawing code...
+}
